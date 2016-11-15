f8cedde03bd05c724f979e64226f65d8d9996b17
TheoVdB <tvand3@ocdsb.ca>
2016-11-15 08:41:52 -0500

    main code

---
+++ src/com/group2/issac/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.group2.issac.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.group2.issac.main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new main(), config);
+	}
+}
